2f618a4cf3a6054c7597a3d7194462ef1416d72a
user149927 <user149927@as417.local>
2019-01-06 02:56:07 +0800

    Initial Commit

---
+++ BeNiMei/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BeNiMei
+//
+//  Created by user149927 on 1/6/19.
+//  Copyright © 2019 Levi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
